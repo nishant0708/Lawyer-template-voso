ec7db2046c943350805f18b70116134e2d058e9a
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2024-07-02 17:01:48 +0530

    updated

--- src/Components/ContactUS/Contact.jsx
+++ src/Components/ContactUS/Contact.jsx
@@ -1,24 +1,35 @@
 import React from 'react'
-import style from "@/Components/Theme5.module.css"
+import styles from "@/Components/Theme5.module.css"
 
 const Contact = ({data}) => {
   return (
-    <div className={style.git}>
-    
-       <div className={style.gitcontainer}>
-        <div className={style.gitimgcontainer}>
-       
-            <img src="/Assets/laywer4.png"  classname={style.gitimg2} alt="" />
+    <div className={styles.appointment_form_main}>
+    <div className={styles['appointment-form']}>
+      <h1 className={styles['appointment-form__title']}>
+        Let's Make An Appointment With Us First
+      </h1>
+      <form className={styles['appointment-form__form']}>
+        <div className={styles.appointment_namephone}>
+        <div className={styles['appointment-form__group appointment_name_div']}>
+          <label className={styles['appointment-form__label']} htmlFor="name">Your Name*</label>
+          <input className={styles['appointment-form__input']} type="text" id="name" name="name" placeholder="Enter your name" required />
         </div>
-        <div className={style.gitformcontainer}>
-           <p>Get In Touch</p>
-            <input type="text" placeholder='Name' />
-            <input type="email" placeholder='Email'/>
-            <input type='tel'  placeholder='Mobile No.'/>
-            <textarea placeholder='Enter Your Message'></textarea>
-            <button>Submit</button>
+        <div className={styles['appointment-form__group appointment_phone_div']}>
+          <label className={styles['appointment-form__label']} htmlFor="phone">Your Phone*</label>
+          <input className={styles['appointment-form__input']} type="tel" id="phone" name="phone" placeholder="Enter your phone number" required />
         </div>
-       </div>
+        </div>
+        <div className={styles['appointment-form__group']}>
+          <label className={styles['appointment-form__label']} htmlFor="email">Your Email*</label>
+          <input className={styles['appointment-form__input']} type="email" id="email" name="email" placeholder="Enter your email address" required />
+        </div>
+        <div className={styles['appointment-form__group']}>
+          <label className={styles['appointment-form__label']} htmlFor="message">Message (optional)</label>
+          <textarea className={styles['appointment-form__textarea']} id="message" name="message" placeholder="Enter message"></textarea>
+        </div>
+        <button className={styles['appointment-form__button']} type="submit">Make Appointment</button>
+      </form>
+    </div>
     </div>
   )
 }
